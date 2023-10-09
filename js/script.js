09de00a91f04cf4b6f3355f3974c8e4d1530777b
Pavlo Karpenko <karpenkopasha19@outlook.com>
2023-10-09 21:14:33 +0300

    09.10.2023

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function showMessage() {
+    alert("Thanks for the information provided)");
+}
+
+
+function showMessage2() {
+    alert("At the moment, we can't give you more information due to the fact that the designer of this layout didn't provide a page for this(");
+}
