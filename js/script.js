ade84ce65d7fb47ee56ab0864effee38def931fe
Pavlo Karpenko <karpenkopasha19@outlook.com>
2023-10-14 23:52:21 +0300

    create header ( main ) slider

--- js/script.js
+++ js/script.js
@@ -6,3 +6,25 @@ function showMessage() {
 function showMessage2() {
     alert("At the moment, we can't give you more information due to the fact that the designer of this layout didn't provide a page for this(");
 }
+
+/*header add background*/
+let headerBackground = document.querySelector(".header__top");
+
+window.onscroll = function() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        headerBackground.style.backgroundColor = "rgb(1%, 1%, 1%, 0.5)";
+    } else {
+        headerBackground.style.backgroundColor = "rgb(1%, 1%, 1%, 0.0)";
+    }
+  };
+
+/*slider*/
+const swiper = new Swiper(".swiper", {
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    // autoplay: {
+    //     delay: 2500,
+    //     disableOneInternaction: false;
+    // }
+  });
